156da194de46748c8e02c8c8c58016b443e87448
jerojash <javiereduardorh@gmail.com>
2023-04-04 14:30:50 -0400

    Initializing project notes_app

---
+++ lib/notes_app.dart
@@ -0,0 +1,11 @@
+/// notes_app
+///
+/// A conduit web server.
+library notes_app;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:conduit_core/conduit_core.dart';
+
+export 'channel.dart';
